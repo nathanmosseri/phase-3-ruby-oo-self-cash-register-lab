5887b91c6f1a8e2dba73c49a9ed2ddf6dff1fc95
Nathan <namo678@me.com>
2022-08-31 15:20:49 -0400

    commit tree 633a92017a68ff2df68f79603b1167be20a9b5dd

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister 
 
+    attr_accessor :total, :discount, :item, :price
+
+   def initialize(discount=nil)
+    @discount = discount
+    @total = 0
+   end
+
+   def add_item(title, price, quantity)
+    self.total += price
+
+   end
+
+
+
+end
